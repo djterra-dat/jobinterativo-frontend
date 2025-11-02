7bb0bae8b8d8ed291838c817f5682fb1e74c2b63
djterra-dat <danielterrapsicanalista@gmail.com>
2025-11-02 19:23:04 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ padding: 20, fontFamily: 'sans-serif' }}>
+      <h1>Job Interativo</h1>
+      <p>Frontend React conectado ao backend Render!</p>
+    </div>
+  );
+}
